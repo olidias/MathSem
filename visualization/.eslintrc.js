bdd37d13ce59caebbba0f12de3354d990a83b17f
Matt Baumann <matthias-baumann@outlook.com>
2018-05-01 14:16:02 +0200

    First draft slides Matt

--- visualization/.eslintrc.js
+++ visualization/.eslintrc.js
@@ -45,6 +45,14 @@ module.exports = {
       optionalDependencies: ['test/unit/index.js']
     }],
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+    "vue/max-attributes-per-line": [2, {
+      "singleline": 3,
+      "multiline": {
+        "max": 1,
+        "allowFirstLine": false
+      }
+    }]
   }
 }
