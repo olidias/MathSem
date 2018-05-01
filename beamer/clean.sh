5af43b98b48365492d6c61805dd256619a50fdec
Matt Baumann <matthias-baumann@outlook.com>
2018-05-01 08:47:19 +0200

    Beamer presenation template
    Template with all needed beamer files. Additionally, there is a ignore
    file for all intermediate files for tex. These files can be cleaned out
    with the clean.sh shell script.

---
+++ beamer/clean.sh
@@ -0,0 +1,26 @@
+# LaTeX temporary files
+rm *.aux
+rm *.log
+rm *.toc
+
+# PDF output - usually a bad idea to keep this in Git
+# rm *.pdf
+
+# Latexmk
+rm *.fdb_latexmk
+
+# SyncTeX
+rm *.synctex.gz
+
+# LaTeX Beamer
+rm *.snm
+rm *.vrb
+rm *.nav
+rm *.out
+
+# BibTeX
+rm *.bbl
+rm *.blg
+
+# DEBUG files
+rm *.fls
