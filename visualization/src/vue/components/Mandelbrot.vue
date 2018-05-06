56798e157c93b0cb53d805160ce0cfc6885ec1bc
Oli Dias <oli.dias94@gmail.com>
2018-05-06 18:15:22 +0200

    Add missing files

---
+++ visualization/src/vue/components/Mandelbrot.vue
@@ -0,0 +1,23 @@
+<template>
+  <article>
+    <header>
+      <h1>Mandelbrot Gif</h1>
+    </header>
+    <section>
+      <main>
+        <p>
+          <img src="../assets/img/mandelbrot.gif" alt="Mandelbrot gif"/>
+        </p>
+      </main>
+    </section>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Mandelbrot',
+};
+</script>
+
+<style scoped>
+</style>
